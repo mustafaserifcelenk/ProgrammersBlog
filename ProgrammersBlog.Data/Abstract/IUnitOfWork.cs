62c1089bd9278f93397b9b8afbb5605098c3c209
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-07 13:47:00 +0300

    1.5.18 Creating Unit Of Work Interface

---
+++ ProgrammersBlog.Data/Abstract/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Data.Abstract
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // _unitOfWork.Categories.AddAsync(); : Kullanıma örnek
+
+        IArticleRepository Articles { get; }
+        ICategoryRepository Categories { get; }
+        ICommentRepository Comments { get; }
+        IRoleRepository Roles { get; }
+        IUserRepository Users { get; }
+
+        // _unitOfWork.Categories.AddAsync(category);
+        // _unitOfWork.Users.AddAsync(user); 
+        // _unitOfWork.SaveAsync();
+
+        Task<int> SaveAsync(); // <int> etkilenen veri sayısını almak için
+
+    }
+}
