a6727ba3651da2fbe79c6eff8885921a68474831
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-11 17:01:27 +0300

    1.8.38 Creating ServiceCollectionExtensions Class and LoadMyServices Method (Dependency Injection)

---
+++ ProgrammersBlog.Services/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using ProgrammersBlog.Data.Abstract;
+using ProgrammersBlog.Data.Concrete.EntityFramework.Contexts;
+using ProgrammersBlog.Services.Abstract;
+using ProgrammersBlog.Services.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection LoadMyServices(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.AddDbContext<ProgrammersBlogContext>();
+            serviceCollection.AddScoped<IUnitOfWork, IUnitOfWork>();
+            serviceCollection.AddScoped<IArticleService, ArticleManager>();
+            serviceCollection.AddScoped<ICategoryService, CategoryManager>();
+            return serviceCollection;
+        }
+    }
+}
