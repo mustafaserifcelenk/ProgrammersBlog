4d083b1d100647410ec188a62ac7171112715497
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-11 13:32:27 +0300

    1.8.31 Creating IArticleService Interface

---
+++ ProgrammersBlog.Services/Abstract/IArticleService.cs
@@ -0,0 +1,24 @@
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Services.Abstract
+{
+    public interface IArticleService
+    {
+        //Burada işleri biraz daha profesyonelleştirerek article nesnesini yani domain modelimizi dışarı açmadan bu işleri yapmaya çalışacağız
+        Task<IDataResult<ArticleDto>> Get(int articleId);
+        Task<IDataResult<ArticleListDto>> GetAll(); // Get türü işlemler için IDataResult kullanırız
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeleted();
+        Task<IDataResult<ArticleListDto>> GetAllByNonDeletedAndActive();
+        Task<IDataResult<ArticleListDto>> GetAllByCategory(int categoryId);
+        Task<IResult> Add(ArticleAddDto articleAddDto, string createdByName); // Add türü işlemler için IResult kullanırız
+        Task<IResult> Update(ArticleUpdateDto articleUpdateDto, string modifiedByName);
+        Task<IResult> Delete(int articleId, string modifiedByName);
+        Task<IResult> HardDelete(int articleId);
+    }
+}
