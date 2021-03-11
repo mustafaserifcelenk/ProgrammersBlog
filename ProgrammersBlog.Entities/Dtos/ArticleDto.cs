2bf57517dac491db25e0b5c66da8c89f099e187b
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-11 14:07:16 +0300

    1.8.33 Completing ArticleAddDto and ArticleUpdateDto Classes

--- ProgrammersBlog.Entities/Dtos/ArticleDto.cs
+++ ProgrammersBlog.Entities/Dtos/ArticleDto.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ProgrammersBlog.Entities.Concrete;
+using ProgrammersBlog.Shared.Data.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,10 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Dtos
 {
-    public class ArticleDto
+    public class ArticleDto : DtoGetBase
     {
+        // Zaten resultlarda resultstatus'lar olacak neden burda da ekledik? Cevap : Viewlarda da Article yanında ResultStatus'a da ihtiyacımız olacak. Success olup olmamasına göre farklı görünümler isteyeceğiz.
+        public Article Article { get; set; }
+        //public ResultStatus ResultStatus { get; set; }
     }
 }
---
+++ ProgrammersBlog.Shared/Data/Abstract/DtoGetBase.cs
@@ -0,0 +1,14 @@
+﻿using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Shared.Data.Abstract
+{
+    public abstract class DtoGetBase // abstract : Entity'den entity'e buradaki ResultStatus'un default değerle gelmesini isteyebiliriz
+    {
+        public virtual ResultStatus ResultStatus { get; set; }
+    }
+}
