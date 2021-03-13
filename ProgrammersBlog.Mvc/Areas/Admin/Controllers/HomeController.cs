8267b0035f38697cac1fef2560132ad1a5418fef
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-13 10:40:22 +0300

    1.9.40 Creating MVC Structure and Admin Area

---
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Mvc.Area.Admin.Controllers // Yolu düzelttik
+{
+    [Area("Admin")] // Bunu ekledik
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
