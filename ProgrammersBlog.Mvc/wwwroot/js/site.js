8062c165be71d2ff5fca5d0a9f3bb1a589e411ad
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-16 11:55:34 +0300

    1.10.53 Editing AJAX Post Processes

---
+++ ProgrammersBlog.Mvc/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿function convertFirstLetterToUpperCase(text) {
+    return text.charAt(0).toUpperCase() + text.slice(1);
+}
+
+//date stringfy edildiğinden string olarak geliyor
+function convertToShortDate(dateString) {
+    const shortDate = new Date(dateString).toLocaleDateString('en-Us');
+    return shortDate;
+}
