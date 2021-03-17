2a9a2145e765d4db06bf81d8eeba215a865561a0
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-17 16:17:37 +0300

    1.11.84 Creating ImageDelete() Operation

--- ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
+++ ProgrammersBlog.Mvc/Areas/Admin/Controllers/UserController.cs
@@ -162,5 +162,17 @@ namespace ProgrammersBlog.Mvc.Areas.Admin.Controllers
 
             return fileName;
         }
+
+        public bool ImageDelete(string pictureName)
+        {
+            string wwwroot = _env.WebRootPath;
+            var fileToDelete = Path.Combine($"{wwwroot}/img", pictureName);
+            if (System.IO.File.Exists(fileToDelete))
+            {
+                System.IO.File.Delete(fileToDelete);
+                return true;
+            }
+            return false;
+        }
     }
 }
