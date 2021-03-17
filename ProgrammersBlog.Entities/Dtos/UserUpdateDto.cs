e866604655c6ed186c3638646e35be3981752d8d
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-17 15:56:47 +0300

    1.11.82 Creating UserUpdateDto Class and _UserUpdateParial Page

--- ProgrammersBlog.Entities/Dtos/UserUpdateDto.cs
+++ ProgrammersBlog.Entities/Dtos/UserUpdateDto.cs
@@ -9,11 +9,8 @@ using System.Threading.Tasks;
 
 namespace ProgrammersBlog.Entities.Dtos
 {
-    public class UserUpdateDto
+    public class UserAddDto
     {
-        [Required]
-        public int Id { get; set; }
-
         [DisplayName("Kullanıcı Adı")]
         [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
         [MaxLength(50, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
@@ -27,6 +24,13 @@ namespace ProgrammersBlog.Entities.Dtos
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [DisplayName("Şifre")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(6, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
         [DisplayName("Telefon Numarası")]
         [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
         [MaxLength(13, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
@@ -34,11 +38,10 @@ namespace ProgrammersBlog.Entities.Dtos
         [DataType(DataType.PhoneNumber)]
         public string PhoneNumber { get; set; }
 
-        [DisplayName("Resim Ekle")]
+        [DisplayName("Resim")]
+        [Required(ErrorMessage = "Lütfen bir {0} ekleyin.")]
         [DataType(DataType.Upload)]
         public IFormFile PictureFile { get; set; }
-
-        [DisplayName("Resim")]
         public string Picture { get; set; }
     }
 }
