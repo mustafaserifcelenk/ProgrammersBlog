43c414d4574bbf77c3a075ad751848b832c7326d
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-19 10:37:38 +0300

    1.11.87 Refactoring on userIndex.js

--- ProgrammersBlog.Mvc/wwwroot/js/userIndex.js
+++ ProgrammersBlog.Mvc/wwwroot/js/userIndex.js
@@ -35,17 +35,20 @@
                            if (userListDto.ResultStatus === 0) {
                                $.each(userListDto.Users.$values,
                                    function (index, user) {
-                                       dataTable.row.add([
+                                       const newTableRow = dataTable.row.add([
                                            user.Id,
                                            user.UserName,
                                            user.Email,
                                            user.PhoneNumber,
-                                           `<img src="/img/${user.Picture}" alt="${user.UserName}" style="max-height: 50px; max-width: 50px;" />`,
+                                           `<img src="/img/${user.Picture}" alt="${user.UserName}" class="my-image-table" />`,
                                            `
                                 <button class="btn btn-primary btn-sm btn-update" data-id="${user.Id}"><span class="fas fa-edit"></span></button>
                                 <button class="btn btn-danger btn-sm btn-delete" data-id="${user.Id}"><span class="fas fa-minus-circle"></span></button>
                                             `
-                                       ]);
+                                       ]).node(); //node sayesinde add'lediğimiz row'u seçebiliyor oluyoruz
+                                       //foreach içindeyken her rowu jquery objesine dönüştürüyoruz, datatable api'dan jquery objesine geçiş yapmış olduk
+                                       const jqueryTableRow = $(newTableRow);
+                                       jqueryTableRow.attr('name', `${user.Id}`);
                                    });
                                dataTable.draw();
                                $('.spinner-border').hide();
@@ -136,17 +139,20 @@
                         const isValid = newFormBody.find('[name="IsValid"]').val() === 'True';
                         if (isValid) {
                             placeHolderDiv.find('.modal').modal('hide');
-                            dataTable.row.add([
+                            const newTableRow = dataTable.row.add([
                                 userAddAjaxModel.UserDto.User.Id,
                                 userAddAjaxModel.UserDto.User.UserName,
                                 userAddAjaxModel.UserDto.User.Email,
                                 userAddAjaxModel.UserDto.User.PhoneNumber,
-                                `<img src="/img/${userAddAjaxModel.UserDto.User.Picture}" alt="${userAddAjaxModel.UserDto.User.UserName}" style="max-height: 50px; max-width: 50px;" />`,
+                                `<img src="/img/${userAddAjaxModel.UserDto.User.Picture}" alt="${userAddAjaxModel.UserDto.User.UserName}" class="my-image-table" />`,
                                 `
                                 <button class="btn btn-primary btn-sm btn-update" data-id="${userAddAjaxModel.UserDto.User.Id}"><span class="fas fa-edit"></span></button>
                                 <button class="btn btn-danger btn-sm btn-delete" data-id="${userAddAjaxModel.UserDto.User.Id}"><span class="fas fa-minus-circle"></span></button>
                             `
-                            ]).draw();
+                            ]).node();
+                            const jqueryTableRow = $(newTableRow);
+                            jqueryTableRow.attr('name', `${userAddAjaxModel.UserDto.User.Id}`);
+                            dataTable.row(newTableRow).draw();
                             toastr.success(`${userAddAjaxModel.UserDto.Message}`, 'Başarılı İşlem!');
                         } else {
                             let summaryText = "";
@@ -163,9 +169,9 @@
                 });
             });
     });
-    //     Ajax POST / Posting the FormData as CategoryAddDto ends here. 
+    //     Ajax POST / Posting the FormData as UserAddDto ends here. 
 
-    //     Ajax POST / Deleting a Category starts from here. 
+    //     Ajax POST / Deleting a User starts from here. 
     //     document : Sayfadaki sil butonuna tıklandığını anlaması için document diyoruz, "sayfada" anlamına geliyor (sayfa objesini veriyoruz)
     $(document).on('click',
         '.btn-delete',
