0e5d98944da7f4d852252f0049449a94d3bae8bf
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-22 11:48:56 +0300

    2.1.5 Add jQuery UI Library and DatePicker

--- ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
+++ ProgrammersBlog.Mvc/wwwroot/AdminLTE/js/articleAdd.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
             ['emoji'],
             ['fontfamily'],
             ['fontsize']
-        ],
+        ]
         //plugins: {
         //    colors: {
         //        foreColorList: [
@@ -37,11 +37,44 @@ $(document).ready(function () {
 
     // Trumbowyg
 
+    // Select2
 
     $('#categoryList').select2({
         theme: 'bootstrap4',
         placeholder: "Lütfen bir kategori seçiniz",
         allowClear: true
     });
+
+    // Select2
+
+    // jQuery UI - DatePicker
+
+    $(function () {
+        $("#datepicker").datepicker({
+            closeText: "kapat",
+            prevText: "&#x3C;geri",
+            nextText: "ileri&#x3e",
+            currentText: "bugün",
+            monthNames: ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran",
+                "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"],
+            monthNamesShort: ["Oca", "Şub", "Mar", "Nis", "May", "Haz",
+                "Tem", "Ağu", "Eyl", "Eki", "Kas", "Ara"],
+            dayNames: ["Pazar", "Pazartesi", "Salı", "Çarşamba", "Perşembe", "Cuma", "Cumartesi"],
+            dayNamesShort: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            dayNamesMin: ["Pz", "Pt", "Sa", "Ça", "Pe", "Cu", "Ct"],
+            weekHeader: "Hf",
+            dateFormat: "dd.mm.yy",
+            firstDay: 1,
+            isRTL: false,
+            showMonthAfterYear: false,
+            yearSuffix: "", 
+            duration: 750,
+            minDate: -1,
+            maxDate: +1
+
+        });
+    });
+
+    // jQuery UI - DatePicker
 
 })
