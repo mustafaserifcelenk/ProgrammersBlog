3613c53a147e52ea3207b53adfdfb42ecda89a89
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-22 13:24:58 +0300

    2.1.8 Revise ImageHelper/UploadUserImage() Method

--- ProgrammersBlog.Mvc/Helpers/Abstract/IImageHelper.cs
+++ ProgrammersBlog.Mvc/Helpers/Abstract/IImageHelper.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
+using ProgrammersBlog.Entities.ComplexTypes;
 using ProgrammersBlog.Entities.Dtos;
 using ProgrammersBlog.Shared.Utilities.Results.Abstract;
 using System;
@@ -10,7 +11,8 @@ namespace ProgrammersBlog.Mvc.Helpers.Abstract
 {
     public interface IImageHelper 
     {
-        Task<IDataResult<ImageUploadedDto>> UploadUserImage(string userName, IFormFile pictureFile, string folderName = "userImages");
+        // İstersek dosya adı verebiliriz, vermezsek burdaki pictureType'a göre otomatik atama sağlayacağız
+        Task<IDataResult<ImageUploadedDto>> Upload(string name, IFormFile pictureFile, PictureType pictureType, string folderName = null);
         IDataResult<ImageDeletedDto> Delete(string pictureName);
     }
 }
