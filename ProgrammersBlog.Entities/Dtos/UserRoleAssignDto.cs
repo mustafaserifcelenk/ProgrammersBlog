6bd720933aa8e8f7c5e44879861db209ee6d9f47
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-23 14:19:30 +0300

    2.2.27 Create UserRoleAssignDto and RoleAssignDto Classes

---
+++ ProgrammersBlog.Entities/Dtos/UserRoleAssignDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammersBlog.Entities.Dtos
+{
+    public class UserRoleAssignDto
+    {
+        // Eğer bir listeye teker teker eleman atayacaksak bunu başta initialize etmemiz gerekir, ama direk bu IList'e direk liste atayacaksak initialize etmemize gerek olmaz. Bunun sebebi IList olarak bir interface kullanmamızdır.
+        public UserRoleAssignDto()
+        {
+            RoleAssignDtos = new List<RoleAssignDto>();
+        }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public IList<RoleAssignDto> RoleAssignDtos { get; set; }
+    }
+}
