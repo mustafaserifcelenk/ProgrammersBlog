3556d60668488f3831a3519570c8da42bdf5dadd
Mustafa <mustafaserifcelenk@gmail.com>
2021-03-27 16:35:56 +0300

    2.7.74 Create ValidationError Class and Improvement on Result Structure

--- ProgrammersBlog.Shared/Utilities/Results/Concrete/DataResult.cs
+++ ProgrammersBlog.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -1,10 +1,11 @@
-﻿using ProgrammersBlog.Shared.Utilities.Results.Abstract;
-using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ProgrammersBlog.Shared.Entities.Concrete;
+using ProgrammersBlog.Shared.Utilities.Results.Abstract;
+using ProgrammersBlog.Shared.Utilities.Results.ComplexTypes;
 
 namespace ProgrammersBlog.Shared.Utilities.Results.Concrete
 {
@@ -15,14 +16,25 @@ namespace ProgrammersBlog.Shared.Utilities.Results.Concrete
             ResultStatus = resultStatus;
             Data = data;
         }
-
+        public DataResult(ResultStatus resultStatus, T data, IEnumerable<ValidationError> validationErrors)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+            ValidationErrors = validationErrors;
+        }
         public DataResult(ResultStatus resultStatus, string message, T data)
         {
             ResultStatus = resultStatus;
             Message = message;
             Data = data;
         }
-
+        public DataResult(ResultStatus resultStatus, string message, T data, IEnumerable<ValidationError> validationErrors)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+            ValidationErrors = validationErrors;
+        }
         public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
         {
             ResultStatus = resultStatus;
@@ -30,13 +42,18 @@ namespace ProgrammersBlog.Shared.Utilities.Results.Concrete
             Data = data;
             Exception = exception;
         }
-
-        public T Data { get; }
-
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception, IEnumerable<ValidationError> validationErrors)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+            Exception = exception;
+            ValidationErrors = validationErrors;
+        }
         public ResultStatus ResultStatus { get; }
-
         public string Message { get; }
-
         public Exception Exception { get; }
+        public IEnumerable<ValidationError> ValidationErrors { get; set; }
+        public T Data { get; }
     }
 }
